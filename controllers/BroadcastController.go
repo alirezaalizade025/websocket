e647222ae9ec4205dd8b0820a31fce3ebf34b6b9
alireza alizadeh <alirezaalizadeh025@gmail.com>
2024-02-21 12:04:58 +0330

    fix broadcast route

--- controllers/BroadcastController.go
+++ controllers/BroadcastController.go
@@ -16,6 +16,8 @@ type BroadCastStoreRequest struct {
 	Message     string `json:"message" form:"message" binding:"omitempty,max=255"`
 	Action      string `json:"action" form:"action" binding:"omitempty,max=50"`
 	Data        string `json:"data" form:"data" binding:"omitempty,max=1000"`
+	Type        string `json:"type" form:"type" binding:"omitempty,max=50"`
+	AutoClose   int    `json:"auto_close" form:"auto_close" binding:"omitempty,max=10000"`
 }
 
 func Broadcast(c *gin.Context, m *melody.Melody) {
@@ -24,7 +26,7 @@ func Broadcast(c *gin.Context, m *melody.Melody) {
 	request := &BroadCastStoreRequest{
 		ChannelName: c.PostForm("channel_name"),
 		Message:     c.PostForm("message"),
-		Action:      c.PostForm("action"),
+		Action:      c.PostForm("action"), // todo: null check
 		Data:        c.PostForm("data"),
 	}
 
@@ -37,28 +39,35 @@ func Broadcast(c *gin.Context, m *melody.Melody) {
 	}
 
 	// find channel
-	channel := models.Channel{}
-	err := channel.FirstOrCreate(request.ChannelName)
-	if err != nil {
-		c.JSON(422, gin.H{
-			"errors": "Channel not found",
-		})
-		return
+	if request.ChannelName != "" {
+		channel := models.Channel{}
+		err := channel.FirstOrCreate(request.ChannelName)
+		if err != nil {
+			c.JSON(422, gin.H{
+				"errors": "Channel not found",
+			})
+			return
+		}
 	}
 
- 
 	// generate message
 	message, err := json.Marshal(models.Message{
 		ChannelName: request.ChannelName,
 		Action:      request.Action,
-		Data:        request.Data,
+		Data: map[string]interface{}{
+			"message":    request.Message,
+			"type":       request.Type,
+			"auto_close": request.AutoClose,
+		},
 	})
 
 	if err != nil {
 		log.Panicln(err)
 	}
 
-	channel.Broadcast(message, m)
+	m.Broadcast(message)
+
+	// channel.Broadcast(message, m)
 }
 
 // translateError is a helper function to translate error from validator
